67d28b480fc8b08b4a1ed5a65ff5b6438f0e676d
ijungleboy <daniel.mettler@2sic.com>
2021-04-29 20:31:47 +0200

    add messages to Koi

---
+++ Connect.Koi/Messages.cs
@@ -0,0 +1,16 @@
+﻿namespace Connect.Koi
+{
+    public class Messages
+    {
+        public static string Link = "https://connect-koi.net/";
+        public static string CssInformationMissing =
+            $@"This component supports multiple CSS frameworks. \n
+It uses <a href='{Link}' target='_blank'>Koi</a> to detect the CSS framework of the theme/skin. 
+Your page-theme doesn't broadcast it's CSS-Framework, so the output may look wrong. <br> 
+
+To fix this, follow the instructions on <a href='{Link}'>Connect.Koi</a>.";
+
+        public static string OnlyAdminsSeeThis =
+            "Note: only admins see this message, and it will disappear when you configure your page-theme.";
+    }
+}
