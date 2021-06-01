1263b374d9d2307e8b6a244da70a33a035fc0a9d
ijungleboy <daniel.mettler@2sic.com>
2021-06-01 09:11:01 +0200

    minor, not relevant

---
+++ Connect.Koi/Internals/EnforceNamedParameters.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+
+//namespace Connect.Koi.Internals
+//{
+//    /// <summary>
+//    /// Helper to ensure that commands with various overloads must use named parameters
+//    /// </summary>
+//    public class EnforceNamedParameters
+//    {
+//        /// <summary>
+//        /// This is just a constant to give to the parameter "protecting" the others
+//        /// Its value and type can be changed from time to time, as it's value is only checked internally. 
+//        /// </summary>
+//        public const string ProtectionKey = "Dummy-Parameter - don't provide this, but do name all other parameters in this call using paramName: value";
+
+//        public static bool VerifyProtectionKey(string value, bool throwError = true)
+//        {
+//            var valid = value == ProtectionKey;
+//            if (!valid && throwError)
+//                throw new Exception("");
+//            return valid;
+//        }
+//    }
+//}
